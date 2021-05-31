21f6266bf76fd9c5729d7641ff296db126a343fd
岡優志 <yu.j.0513@gmail.com>
2021-05-31 20:57:41 +0900

    記録するボタンと計算するボタンを変更、追加

--- TimeCalculate/TimeCalculate/ViewController.swift
+++ TimeCalculate/TimeCalculate/ViewController.swift
@@ -8,23 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak private var timeLabel: UILabel!
+    @IBOutlet weak private var subtractionTimeLabel: UILabel!
     @IBOutlet weak private var inputTimeTextField: UITextField!
     
     let dateFormatter = DateFormatter()
+    var logDate: Double = 0.0
+    let minutes = 60
+    let hour = 3600
+    let day = 86400
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     @IBAction func upDateButton(_ sender: Any) {
-        dateFormatter.dateFormat = "yyyy/MM/dd/hh:mm:ss"
-            print(String(dateFormatter.string(from: Date())))
+        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        print(String(dateFormatter.string(from: Date())))
         let nowDate = Date()
         print(nowDate)
         let nowDateFormat = nowDate.timeIntervalSinceNow
         print(nowDate)
         let toDay = nowDateFormat / 86400
+        logDate = toDay
         print(toDay)
+        print(logDate)
+    }
+    @IBAction func subtractionCalculateButton(_ sender: Any) {
     }
 }
 
+
+enum DateStore {
+    case second
+    case minutes
+    case hour
+    case day
+    case week
+    case month
+    case year
+}
