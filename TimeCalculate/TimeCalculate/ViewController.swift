5269e1e705c7b023551258cee06563ebb056472c
岡優志 <yu.j.0513@gmail.com>
2021-05-31 23:02:06 +0900

    記録した日時と現在の日時の差分を取り、labelに表示するようにしました

--- TimeCalculate/TimeCalculate/ViewController.swift
+++ TimeCalculate/TimeCalculate/ViewController.swift
@@ -10,29 +10,23 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak private var subtractionTimeLabel: UILabel!
     @IBOutlet weak private var inputTimeTextField: UITextField!
-    
-    let dateFormatter = DateFormatter()
-    var logDate: Double = 0.0
-    let minutes = 60
-    let hour = 3600
-    let day = 86400
-    
+
+    var logDate = Date()
+    var nowDate = Date()
+    var subtraction = 0
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func upDateButton(_ sender: Any) {
-        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
-        print(String(dateFormatter.string(from: Date())))
-        let nowDate = Date()
-        print(nowDate)
-        let nowDateFormat = nowDate.timeIntervalSinceNow
-        print(nowDate)
-        let toDay = nowDateFormat / 86400
-        logDate = toDay
-        print(toDay)
-        print(logDate)
+    
+    @IBAction private func upDateButton(_ sender: Any) {
+        logDate = Date()
     }
-    @IBAction func subtractionCalculateButton(_ sender: Any) {
+    
+    @IBAction private func subtractionCalculateButton(_ sender: Any) {
+        nowDate = Date()
+        let subtractionSecond = nowDate.timeIntervalSince(logDate)
+        subtraction = Int(subtractionSecond)
+        subtractionTimeLabel.text = String(subtraction)
     }
 }
 
@@ -46,3 +40,7 @@ enum DateStore {
     case month
     case year
 }
+
+//let dateFormatter = DateFormatter()
+//dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+//print(dateFormatter.string(from: Date()))
