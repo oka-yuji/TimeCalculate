890a66156013450c2dfbcd19183a3a7dd3aa0e32
岡優志 <yu.j.0513@gmail.com>
2021-05-31 20:26:19 +0900

    ボタンタップde

--- TimeCalculate/TimeCalculate/ViewController.swift
+++ TimeCalculate/TimeCalculate/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak private var timeLabel: UILabel!
+    @IBOutlet weak private var inputTimeTextField: UITextField!
+    
+    let dateFormatter = DateFormatter()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func upDateButton(_ sender: Any) {
+        dateFormatter.dateFormat = "yyyy/MM/dd/hh:mm:ss"
+            print(String(dateFormatter.string(from: Date())))
+        let nowDate = Date()
+        print(nowDate)
+        let nowDateFormat = nowDate.timeIntervalSinceNow
+        print(nowDate)
+        let toDay = nowDateFormat / 86400
+        print(toDay)
+    }
 }
 
