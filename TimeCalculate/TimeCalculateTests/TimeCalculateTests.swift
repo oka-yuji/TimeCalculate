b39bfb127996c76c7d12ce918d0d65ba5071cea4
岡優志 <yu.j.0513@gmail.com>
2021-06-03 00:01:08 +0900

    コメントを追加しました

--- TimeCalculate/TimeCalculateTests/TimeCalculateTests.swift
+++ TimeCalculate/TimeCalculateTests/TimeCalculateTests.swift
@@ -10,11 +10,11 @@ import XCTest
 
 class TimeCalculateTests: XCTestCase {
     let isDate = IsDate(calculate: SubtractionCalculate())
-    let testLogDate = "2021-01-01 12:00:00"
+    // 現在の時間を設定
     let testNowDate = "2021-01-01 12:00:00"
-    let dateFormat = DateFormatter()
+    // testLogDate=テスト用のDate reslut=期待する結果
     let testDates: [(testLogDate: String, reslut: String)] = [
-        ("2021-01-01 11:59:59", "1秒以内"),
+        ("2021-01-01 11:59:30", "30秒以内"),
         ("2021-01-01 11:58:30", "1分以内"),
         ("2021-01-01 10:30:20", "1時間以内"),
         ("2020-12-31 11:50:00", "1日以内"),
@@ -23,12 +23,13 @@ class TimeCalculateTests: XCTestCase {
         ("2020-01-01 11:59:59", "1年以内"),
         ("2022-01-01 11:59:59", "エラー")
     ]
-    
+    let dateFormat = DateFormatter()
+    // ここからテスト
     func testDate() {
         for testDate in 0..<testDates.count {
+            // String型で作ったテスト用のDateをDate型に
             dateFormat.dateFormat = "yyyy-MM-dd HH:mm:ss"
             dateFormat.locale = Locale(identifier: "ja_JP")
-//            let logDate = dateFormat.date(from: testLogDate) ?? Date()
             let logDate = dateFormat.date(from: testDates[testDate].testLogDate) ?? Date()
             let nowDate = dateFormat.date(from: testNowDate) ?? Date()
             let reslut: String = isDate.isDate(isLogDate: logDate, isNowDate: nowDate)
